835ce368ff26d92d48bb50947d432b9a3c1233d8
Abhishek Bante <abhishek.bante.cse@ghrce.raisoni.net>
2023-04-23 10:20:35 +0530

    created signin and signup pages

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const path = require('path')
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
+
+app.get('/',(request, response) =>{
+    response.render('index');
+});
+
+app.get('/login',(request, response) => {
+    response.render('signin');
+});
+app.get('/signup',(request, response) => {
+    response.render('signup');
+});
 
 
 module.exports = app
